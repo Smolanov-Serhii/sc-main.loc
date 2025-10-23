9cc3ef4ddaca1a7929ee69193ebe76be2322e823
smolanovsergej <smolanovsergey@gmail.com>
2025-10-23 07:34:34 +0300

    new template with vertical banner

---
+++ wp-content/themes/sc-stom/implant-new.php
@@ -0,0 +1,26 @@
+<?php
+/* Template Name: Шаблон услуг (верт банер) */
+$post_id = get_the_ID();
+get_header();
+?>
+    <main class="main imp">
+        <?php get_template_part( 'template-parts/content', 'imp-one' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-two' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-three' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-four' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-five' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-six' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-seven' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-eight' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-nine' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-ten' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-eleven' ); ?>
+        <?php get_template_part( 'template-parts/content', 'imp-tv' ); ?>
+        <?php get_template_part( 'template-parts/content', 'map' ); ?>
+    </main>
+<?php
+get_footer();
+
+
+
+
---
+++ wp-content/themes/sc-stom/template-parts/content-imp-one-vert.php
@@ -0,0 +1,45 @@
+<?php
+$post_id = get_the_ID();
+?>
+<section class="imp-one-vert">
+    <div class="imp-one-vert__container main-container">
+        <h1 class="imp-one-vert__title section-title">
+            <?php echo get_field('zagolovok_bloka_banner', $post_id)?>
+        </h1>
+        <div class="imp-one-vert__subtitle section-subtitle">
+            <?php echo get_field('podzagolovok_bloka_banner', $post_id)?>
+        </div>
+        <div class="imp-one-vert__btns">
+            <div class="imp-one-vert__btn button button-blue js-messenger">
+                <span><?php echo get_field('tekst_tmnoj_knopky', $post_id)?></span>
+            </div>
+            <div class="imp-one-vert__btn button button-white js-zapis">
+                <span><?php echo get_field('tekst_svetloj_knopky', $post_id)?></span>
+            </div>
+        </div>
+        <div class="imp-one-vert__slider swiper-container">
+            <?php
+            if( have_rows('slajder_bannera',$post_id) ):
+                echo '<div class="imp-one-vert__wrapper swiper-wrapper">';
+                while( have_rows('slajder_bannera',$post_id) ) : the_row();
+                    $image = get_sub_field('yzobrazhenye_slajdera');
+                    ?>
+                    <div class="imp-one-vert__slider-item swiper-slide">
+                        <img src="<?php echo $image;?>">
+                    </div>
+                <?php
+                endwhile;
+                while( have_rows('slajder_bannera',$post_id) ) : the_row();
+                    $image = get_sub_field('yzobrazhenye_slajdera');
+                    ?>
+                    <div class="imp-one-vert__slider-item swiper-slide">
+                        <img src="<?php echo $image;?>">
+                    </div>
+                <?php
+                endwhile;
+                echo '</div>';
+            endif;
+            ?>
+        </div>
+    </div>
+</section>
